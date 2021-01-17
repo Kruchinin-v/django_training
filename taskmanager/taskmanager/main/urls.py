ec7da1d601221e82cc842ae083bd31d3a1ac8f6d
Kruchinin-v <vladislav97kr@gmail.com>
2021-01-18 01:02:41 +0300

    сделал по видео

--- taskmanager/taskmanager/main/urls.py
+++ taskmanager/taskmanager/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
+    path('create', views.create, name='create'),
 ]
--- taskmanager/taskmanager/main/views.py
+++ taskmanager/taskmanager/main/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import TaskForm
+from .models import Task
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    # tasks = Task.objects.all()
+    # tasks = Task.objects.order_by('id')
+    # tasks = Task.objects.order_by('-title')
+    # tasks = Task.objects.order_by('title')[:3]
+    tasks = Task.objects.order_by('title')
+    return render(request, 'main/index.html', {'title': "Главная страница", 'tasks': tasks})
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    ip = request.META.get('REMOTE_ADDR')
+    return render(request, 'main/about.html', {'ip_address': ip})
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Не верно заполненно"
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error,
+    }
+    return render(request, 'main/create.html', context)
