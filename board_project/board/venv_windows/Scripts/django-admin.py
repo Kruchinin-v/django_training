b0366c3352b733b8c8145c4ce63780cbd955bc57
Kruchinin-v <vladislav97kr@gmail.com>
2021-01-21 16:06:15 +0300

    delete

--- board_project/board/venv_windows/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\vkruchinin\documents\projects\python\django_training\board_project\board\venv_windows\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
